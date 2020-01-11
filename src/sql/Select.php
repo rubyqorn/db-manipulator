ac012918831775c18efaf85d3225cee072d46cb4
Anton Hideger <antonhideger1337@gmail.com>
2020-01-11 16:34:45 +0300

    was realized condition and join methods in select class

--- src/sql/Select.php
+++ src/sql/Select.php
@@ -97,4 +97,171 @@ class Select extends Query
         return $this;
     }
 
+    /**
+     * This is equivalent of ORDER BY statement in SQL
+     * 
+     * Pass our SQL statement into sql array and
+     * return references on this object
+     * 
+     * @param string $column 
+     * @param string $sortOrder
+     * 
+     * @return \Manipulator\SQL\Select
+     */ 
+    public function orderBy(string $column, string $sortOrder)
+    {
+        $this->sql[] = " ORDER BY {$column} {$sortOrder}";
+        return $this;
+    } 
+
+    /**
+     * This is equivalent of DISTINCT sorter in SQL
+     * 
+     * Pass SQL statement into sql array and return 
+     * reference on this object
+     * 
+     * @param string $columns
+     * 
+     * @return \Manipulator\SQL\Select
+     */ 
+    public function distinct(string $columns)
+    {
+        $this->sql[] = "SELECT DISTINCT {$columns} FROM {$this->table}";
+        return $this;
+    }
+
+    /**
+     * This is equivalent of LIMIT SQL statement
+     * 
+     * Insert into sql array your SQL statement 
+     * and return reference on this object
+     * 
+     * @param string $quantity
+     * 
+     * @return \Manipulator\SQL\Select
+     */ 
+    public function limit(string $quantity)
+    {
+        $this->sql[] = " LIMIT {$quantity}";
+        return $this;
+    }
+
+    /**
+     * This is equivalent of AND SQL statement
+     * 
+     * Can be used with WHERE, BETWEEN and etc... statements
+     * 
+     * @param string $condition 
+     * 
+     * @return \Manipulator\SQL\Select
+     */ 
+    public function and(string $condition)
+    {
+        $this->sql[] = " AND {$condition}";
+        return $this;
+    }
+
+    /**
+     * This is equivalent of OR SQL statement
+     * 
+     * Can be used with another SQL statements
+     * like WHERE and etc
+     * 
+     * @param string $condition 
+     * 
+     * @return \Manipulator\SQL\Select
+     */ 
+    public function or(string $condition)
+    {
+        $this->sql[] = " OR {$condition}";
+        return $this;
+    }
+
+    /**
+     * This is equivalent of BETWEEN SQL statement
+     * 
+     * Usually its use when you need to get average value
+     * 
+     * @param string $column 
+     * @param int  $lowItem
+     * @param int $highItem
+     * 
+     * @return \Manipualator\SQL\Select
+     */ 
+    public function between(string $column, int $lowItem, int $highItem)
+    {
+        $this->sql[] = " WHERE {$column} BETWEEN {$lowItem} AND {$highItem}";
+        return $this;
+    }
+
+    /**
+     * This is equivalent of NOT SQL statement
+     * 
+     * Can be used with IN SQL statement
+     * 
+     * @return \Manipulator\SQL\Select
+     */ 
+    public function not()
+    {
+        $this->sql[] = " NOT";
+        return $this;
+    }
+
+    /**
+     * This is equivalent of IN SQL statement
+     * 
+     * @param string $condition 
+     * 
+     * @return \Manipualtor\SQL\Select
+     */ 
+    public function in(string $condition)
+    {
+        $this->sql[] = " IN ('{$condition}')";
+        return $this;
+    }
+
+    /**
+     * Equivalent of INNER JOIN SQL statement
+     * 
+     * @param string $columns 
+     * @param string $joinedTable 
+     * @param string $condition 
+     * 
+     * @return \Manipulator\SQL\Select
+     */ 
+    public function join(string $columns, string $joinedTable, string $condition)
+    {
+        $this->sql[] = "SELECT {$columns} FROM {$this->table} INNER JOIN {$joinedTable} ON {$condition}";
+        return $this;
+    }
+
+    /**
+     * Equivalent of LEFT JOIN SQL statement
+     * 
+     * @param string $columns 
+     * @param string $joinedTable 
+     * @param string $condition 
+     * 
+     * @return \Manipulator\SQL\Select
+     */ 
+    public function leftJoin(string $columns, string $joinedTable, string $condition)
+    {
+        $this->sql[] = "SELECT {$columns} FROM {$this->table} LEFT JOIN {$joinedTable} ON {$condition}";
+        return $this;
+    }
+
+    /**
+     * Equivalent of RIGHT JOIN SQL statement
+     * 
+     * @param string $columns 
+     * @param string $joinedTable 
+     * @param string $condition 
+     * 
+     * @return \Manipulator\SQL\Select
+     */ 
+    public function rightJoin(string $columns, string $joinedTable, string $condition)
+    {
+        $this->sql[] = "SELECT {$columns} FROM {$this->table} RIGHT JOIN {$joinedTable} ON {$condition}";
+        return $this;
+    }
 }
